6b9d241270208f6f7aa14b489b7c39ec0d79bbcd
Laurence Lutz <laurence.lutz@pole-emploi.fr>
2017-03-09 14:36:10 +0100

    découverte-dates

--- src/main/java/co/simplon/dates/DecouverteDates.java
+++ src/main/java/co/simplon/dates/DecouverteDates.java
@@ -1,42 +1,49 @@
 package co.simplon.dates;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class DecouverteDates {
 
-	public DecouverteDates() {
-		super();
+	public static void main (String[] args) {
+		
+	}
+	public boolean estInferieurDateCourante(Date date) {
+        Date aujourdhui = new Date();
+        return date.before(aujourdhui);
 	}
 
-	public boolean estInferieurDateCourante(Date date) throws ParseException {
-		// TODO
-		throw new RuntimeException("à implémenter");
+	public Date construireDate(String chaineFournie) throws ParseException {
+		DateFormat df = new SimpleDateFormat("dd/MM/yy");
+		return df.parse(chaineFournie);
 	}
 
-	public Date construireDate(String chaineFournie) {
-		// TODO
-		throw new RuntimeException("à implémenter");
-	}
-
-	public Date augmenterDate(Date dateInitiale, int nombreJours,
-			int nombreHeures, int nombreMinutes) {
-		// TODO
-		throw new RuntimeException("à implémenter");
+	public Date augmenterDate(Date dateInitiale, int nombreJours, int nombreHeures, int nombreMinutes) {
+      Calendar calendrier = Calendar.getInstance();
+      calendrier.setTime(dateInitiale);
+      calendrier.add(Calendar.DATE, nombreJours);
+      calendrier.add(Calendar.HOUR, nombreHeures);
+      calendrier.add(Calendar.MINUTE,nombreMinutes );        
+      return calendrier.getTime();
+		
 	}
 
 	public String formaterUneDate(Date date) {
-		// TODO
-		return "";
+		SimpleDateFormat formaterDate = new SimpleDateFormat("dd_MM_yyyy");
+		return formaterDate.format(date);
+	}
+		
+	public String formaterUneHeure(Date time) {
+		SimpleDateFormat formaterHeure = new SimpleDateFormat("HH'h'mm'min'ss'sec'");
+			return formaterHeure.format(time);
 	}
 
-	public String formaterUneHeure(Date time) {
-		// TODO
-		return "";
-	}
-	
 	public String formaterDateEnFrancais(Date date) {
-		// TODO
-		return "";
+		SimpleDateFormat formaterDate = new SimpleDateFormat("EEEE DD MMMM yyyy", Locale.FRENCH);
+		return formaterDate.format(date);
 	}
 }
